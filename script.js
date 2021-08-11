afee7604e4430b6a4bf291f691785f2e368755f1
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-08-11 18:57:32 +0530

    handle the add Click and toggle done and not-done for list items

---
+++ script.js
@@ -0,0 +1,21 @@
+let add = document.getElementById("add");
+let cleanup = document.getElementById("cleanup");
+let sort = document.getElementById("sort");
+let reset = document.getElementById("reset");
+let inputElement = document.getElementById("input");
+let list = document.getElementById("list");
+
+const addHandleClick = () => {
+  let val = inputElement.value;
+  let newTask = document.createElement("li");
+  newTask.innerText = val;
+  newTask.classList.add("listItem");
+  list.appendChild(newTask);
+  inputElement.value = "";
+  newTask.onclick = () => {
+    console.log("clicked");
+    newTask.classList.toggle("disabled");
+  };
+};
+
+add.onclick = addHandleClick;
